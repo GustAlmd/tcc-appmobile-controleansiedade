403d0b3f1ed5cd57aa23002ebcb9aeb60507a7f4
zdeley <drey.santos2005@gmail.com>
2023-06-09 18:23:11 -0300

    atualizacoes jogo

--- src/pages/Games/ansiedadeGame.js
+++ src/pages/Games/ansiedadeGame.js
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from 'react';
 import { View, Animated, Easing, TouchableOpacity, Text, StyleSheet, Platform } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
@@ -6,124 +5,116 @@ import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from '@react-navigation/native';
 
 const App = () => {
-  const navigation = useNavigation();
-  const [textAnim] = useState(new Animated.Value(0));
-  const [isAnimating, setIsAnimating] = useState(false);
+    const navigation = useNavigation();
+    const [breathAnim] = useState(new Animated.Value(0));
+    const [isAnimating, setIsAnimating] = useState(false);
+    const [breathText, setBreathText] = useState('');
 
-  useEffect(() => {
-    startAnimation();
-  }, []);
+    useEffect(() => {
+        pauseBreathing();
+    }, []);
 
-  const startAnimation = () => {
-    setIsAnimating(true);
-    Animated.loop(
-      Animated.sequence([
-        Animated.delay(4000),
-        Animated.timing(textAnim, {
-          toValue: 1,
-          duration: 1000,
-          easing: Easing.ease,
-          useNativeDriver: true,
+    const startBreathing = () => {
+        setIsAnimating(true);
+        animateBreath('Inspire', 4000, () => {
+            animateBreath('Segure a Respiração', 3000, () => {
+                animateBreath('Expire', 4000, () => {
+                    setIsAnimating(false);
+                    setBreathText('');
+                });
+            });
+        });
+    };
+
+    const animateBreath = (text, duration, callback) => {
+        setBreathText(text);
+        Animated.timing(breathAnim, {
+            toValue: 1,
+            duration: duration,
+            easing: Easing.inOut(Easing.ease),
+            useNativeDriver: true,
+        }).start(() => {
+            setBreathText('');
+            breathAnim.setValue(0); // Redefine o valor da animação para 0
+            callback && callback();
+        });
+    };
+
+    const pauseBreathing = () => {
+        setIsAnimating(false);
+        breathAnim.stopAnimation(); // Interrompe a animação atual
+        breathAnim.setValue(0); // Redefine o valor da animação para 0
+    };
+
+    const breathStyle = {
+        opacity: breathAnim.interpolate({
+            inputRange: [0, 1],
+            outputRange: [1, 0.5],
         }),
-        Animated.delay(3000),
-        Animated.timing(textAnim, {
-          toValue: 2,
-          duration: 1000,
-          easing: Easing.ease,
-          useNativeDriver: true,
-        }),
-        Animated.delay(3000),
-        Animated.timing(textAnim, {
-          toValue: 3,
-          duration: 1000,
-          easing: Easing.ease,
-          useNativeDriver: true,
-        }),
-        Animated.delay(4000),
-      ]),
-    ).start();
-  };
+        transform: [
+            {
+                scale: breathAnim.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [1, 1.5],
+                }),
+            },
+        ],
+    };
 
-  const getText = () => {
-    return textAnim.interpolate({
-      inputRange: [0, 1, 2, 3],
-      outputRange: ['Relaxa', 'Inspire', 'Segure a respiração', 'Expire'],
-    });
-  };
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <TouchableOpacity onPress={() => { navigation.navigate('Games');}}>
+                    <Ionicons name={Platform.OS === 'ios' ? 'ios-arrow-back' : 'md-arrow-back'} size={24} color="white" />
+                </TouchableOpacity>
+            </View>
 
-  const breathStyle = {
-    opacity: textAnim.interpolate({
-      inputRange: [0, 1, 2, 3],
-      outputRange: [1, 1, 1, 0],
-    }),
-    transform: [
-      {
-        scale: textAnim.interpolate({
-          inputRange: [0, 1, 2, 3],
-          outputRange: [1, 1, 1.5, 1],
-        }),
-      },
-    ],
-  };
+            <View style={styles.main}>
+                <Animated.View
+                    style={{
+                        width: 200,
+                        height: 200,
+                        borderRadius: 100,
+                        backgroundColor: '#5086c1',
+                        ...breathStyle,
+                    }}
+                />
 
-  const toggleAnimation = () => {
-    if (isAnimating) {
-      textAnim.stopAnimation();
-      setIsAnimating(false);
-    } else {
-      startAnimation();
-      setIsAnimating(true);
-    }
-  };
+                <Text style={styles.breathText}>{breathText}</Text>
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <TouchableOpacity onPress={() => navigation.navigate('Games')}>
-          <Ionicons name={Platform.OS === 'ios' ? 'ios-arrow-back' : 'md-arrow-back'} size={24} color="white" />
-        </TouchableOpacity>
-      </View>
-
-      <View style={styles.main}>
-        <Animated.View
-          style={{
-            width: 200,
-            height: 200,
-            borderRadius: 100,
-            backgroundColor: '#5086c1',
-            ...breathStyle,
-          }}
-        >
-          <Text style={styles.text}>{getText()}</Text>
-        </Animated.View>
-
-        <TouchableOpacity
-          onPress={toggleAnimation}
-          style={styles.touchableContainer}
-        >
-          <Text style={{ color: 'white', fontWeight: 'bold' }}>
-            {isAnimating ? 'Pausar' : 'Começar'}
-          </Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
+                <TouchableOpacity
+                    onPress={isAnimating ? pauseBreathing : startBreathing}
+                    style={{
+                        height: hp('5%'),
+                        width: wp('20%'),
+                        padding: 10,
+                        backgroundColor: '#5086c1',
+                        borderRadius: 5,
+                        marginTop: 100,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}>
+                    <Text style={{ color: 'white', fontWeight: 'bold' }}>
+                        {isAnimating ? 'Pausar' : 'Começar'}
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
 };
 
+export default App;
 
 const styles = StyleSheet.create({
-
     container: {
         backgroundColor: '#556aa9',
         flex: 1
     },
-
     main: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
     },
-
     header: {
         position: 'absolute',
         width: wp('100%'),
@@ -132,13 +123,10 @@ const styles = StyleSheet.create({
         marginStart: wp('5%'),
         backgroundColor: 'transparent',
     },
-
-    touchableContainer: {
-        flex: 1,
-        alignItems: 'flex-end',
-        marginEnd: wp('4%')
+    breathText: {
+        color: 'white',
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 20,
     },
-
 });
-
-export default App;
